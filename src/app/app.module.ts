748da6b846aaefab6c573fbfec69f3520fe6e323
nayfin <natjkern@gmail.com>
2017-08-18 23:29:55 -0500

    Barreled the material modules, added button for adding individual recipe ingredients to the shopping list, updated package.json, deleted unused buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,54 +1,30 @@
+// Libraries
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
-// TODO: Move all MdModules to own barrel module
-import { MdToolbarModule,
-         MdIconModule,
-         MdAutocompleteModule,
-         MdDatepickerModule,
-         MdInputModule,
-         MdRadioModule,
-         MdSelectModule,
-         MdSliderModule,
-         MdSlideToggleModule,
-         MdMenuModule,
-         MdSidenavModule,
-         MdListModule,
-         MdGridListModule,
-         MdCardModule,
-         MdTabsModule,
-         MdButtonModule,
-         MdButtonToggleModule,
-         MdChipsModule,
-         MdProgressSpinnerModule,
-         MdProgressBarModule,
-         MdDialogModule,
-         MdTooltipModule,
-         MdSnackBarModule,
-         } from '@angular/material';
-
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import 'hammerjs';
-
+// Barrels
+import { MaterialModule } from './material.module';
+// App Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipesHomeComponent } from './recipes/recipes-home/recipes-home.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-
+// Services
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipesService } from './recipes/recipes.service';
-
+// Routing
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
@@ -70,32 +46,9 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     HttpModule,
     FlexLayoutModule,
     BrowserAnimationsModule,
-    // TODO: Move all MdModules to own barrel module
-    MdToolbarModule,
-    MdIconModule,
-    MdAutocompleteModule,
-    MdDatepickerModule,
-    MdInputModule,
-    MdRadioModule,
-    MdSelectModule,
-    MdSliderModule,
-    MdSlideToggleModule,
-    MdMenuModule,
-    MdSidenavModule,
-    MdListModule,
-    MdGridListModule,
-    MdCardModule,
-    MdTabsModule,
-    MdButtonModule,
-    MdButtonToggleModule,
-    MdChipsModule,
-    MdProgressSpinnerModule,
-    MdProgressBarModule,
-    MdDialogModule,
-    MdTooltipModule,
-    MdSnackBarModule,
     // Pull in our RoutingModule
     AppRoutingModule,
+    MaterialModule
   ],
   providers: [
     ShoppingListService,
